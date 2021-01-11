2d3f5310ccb9e9e669c96540f369c91f7b0c8ed8
Bellergy RC <rc@bellergy.com>
2021-01-11 13:56:37 +0800

    add lazy load sample

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import _ from 'lodash';
 import './style.scss';
 
 function component () {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'Michael!'], ' ');
-    element.innerHTML += '<br> We are in ' + process.env.NODE_ENV + ' mode.';
-    return element;
+    const element = document.createElement('div')
+    const br = document.createElement('br')
+
+    // Sample of using the imported module 'lodash'
+    element.innerHTML = _.join(['Hello', 'Webpack!'], ' ')
+    element.appendChild(br)
+
+    // Sample of getting the environment mode
+    element.innerHTML += 'We are in ' + process.env.NODE_ENV + ' mode.'
+    element.appendChild(br)
+
+    // Sample of Lazy load module
+    // https://webpack.docschina.org/guides/lazy-loading/
+    const button = document.createElement('button')
+    element.appendChild(button)
+    button.innerHTML = 'Click me to load the print module.'
+    button.onclick = e => import('./print').then(module => {
+        const print = module.default
+        print()
+    })
+    return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
