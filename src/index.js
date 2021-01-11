460918972bbfe4a0e35fb897b399a712d2ecd38c
Bellergy RC <rc@bellergy.com>
2021-01-11 13:26:32 +0800

    add production config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash';
 import './style.scss';
 
-function component() {
+function component () {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'Michael!'], ' ');
+    element.innerHTML += '<br> We are in ' + process.env.NODE_ENV + ' mode.';
     return element;
 }
 
---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'My Page',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
