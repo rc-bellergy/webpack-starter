52f1e60ba9421fef17e06dbd982d6bd0416a71a2
Bellergy RC <rc@bellergy.com>
2021-01-27 18:39:02 +0800

    create a empty template

--- src/index/app.js
+++ src/index/app.js
@@ -1,38 +1,5 @@
 import _ from 'lodash'
-import { Carousel } from 'bootstrap'
 import './style.scss'
-import Home01 from './images/home01.jpg'
-import Home02 from './images/home02.jpg'
-import Home03 from './images/home03.jpg'
-import Home04 from './images/home04.jpg'
-
-// Sample of create bootstrap component 
-const items = [{
-    src: Home01,
-    active: 'active'
-}, {
-    src: Home02,
-    active: ''
-}, {
-    src: Home03,
-    active: ''
-}, {
-    src: Home04,
-    active: ''
-},]
-const template = require('../templates/carousel.ejs')
-const holder = document.querySelector('#CarouselHolder .carousel-inner')
-items.forEach((item) => {
-    holder.innerHTML += template({ item: item })
-})
-const myCarousel = document.querySelector('#CarouselHolder')
-const carousel = new Carousel(myCarousel)
-
-// Sample of getting the environment mode
-const br = document.createElement('br')
-const holder2 = document.getElementById('AnotherHolder')
-holder2.innerHTML += 'We are in ' + process.env.NODE_ENV + ' mode.'
-holder2.appendChild(br)
 
 // Sample of Lazy load module
 // https://webpack.docschina.org/guides/lazy-loading/
@@ -42,4 +9,4 @@ button.onclick = e => import('./another').then(module => {
     const print = module.default
     print()
 })
-holder2.appendChild(button)
+document.body.appendChild(button)
